818ee668a182c0aca87049463cac73cccff31b79
markel-kurti <102586764+markel-kurti@users.noreply.github.com>
2022-04-05 10:49:28 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,3 @@
+var Marcoo = prompt("Sa Followersa ju interesojne?")
+var MarcooCount = Marcoo.length;
+alert("Na kontaktoni ne Instagram ose ne numrin tone te celularit per me shume informacione. " + " Keni shkruar "  + " nga "  + " fjale te mbetura. ");
